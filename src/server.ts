23d11ef21e11f45fb37d6371a8a7fd868e204a97
Ariane Mateus <ariane.ccomp@gmail.com>
2020-07-28 16:01:13 -0300

    feat: config debug

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,6 @@ const app = express();
 
 app.get('/', (request, response) => response.json({ message: ' Hello dev' }));
 
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('Server started on port 3333');
+});
